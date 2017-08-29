79873707cedc08f8b95b5aea1c1702af616f52e8
mildewyPrawn <galeanaara@ciencias.unam.mx>
2017-08-29 18:00:33 -0500

    haskell chido

--- EjerSem/EjerSem01/Combinacion.hs
+++ EjerSem/EjerSem01/Combinacion.hs
@@ -1,22 +1,41 @@
-{-
-- Modelado y Programación 2018-1
-- Semanal: Combinaciones
--}
+--Biblioteca para leer entradas
+import Text.Read
+
+--Modelado y programaci+on 2018-1
+--Semanal 01: Programa que implementa la solución de 
+--la combinación de n en r.
+
+--Función factorial.
+fac :: Int -> Int
+fac 0 = 1
+fac 1 = 1
+fac n = n*(fac (n-1))
+
+--Función de combinaciones de n en r.
+comb :: (Int,Int) -> Int
+comb (n,r) = if ((r <= n) && (n <= 1000))
+               then (div (fac n) ((fac (n-r))*(fac r)))
+               else error ((show r)++" debe ser menor o igual a "++(show n))
+
+--Función que solicita al usuario un par de números enteros.
+obtenInt :: IO (Maybe (Int,Int))
+obtenInt = do
+  putStrLn "Ingresa el primer entero:"
+  line <- getLine
+  case readMaybe line of
+    Just x -> do
+      putStrLn "Ingresa el segundo entero:"
+      line2 <- getLine
+      case readMaybe line2 of
+        Just y -> return (Just (x,y))
+        Nothing -> return Nothing
+    Nothing -> return Nothing
 
 
---Función que devuelve el factorial de un entero n
-
-factorial :: Integer->Integer
-factorial 0 = 1
-factorial n = n * factorial (n-1)
-
---Función que devuelve la combinación de dos enteros n k
-
-comb :: Integer -> Integer -> Integer
-comb n r = (factorial n)`div` (factorial( n-r)* factorial(r))
-
-
-
-    
-
-
+--Función principal
+main = do
+  n <- obtenInt
+  case n of
+    Just (n,r) -> let res = comb (n,r) in
+      putStrLn ("Resultado: "++(show res))
+    Nothing -> error "Argumentos inválidos."
